7d162fbc788aa82b722afce088dc956b97c58951
arjunxmishra525 <arjun.x.mishra525@gmail.com>
2026-08-07 02:34:00 +0530

    Update backend.tf

--- environment/dev/backend.tf
+++ environment/dev/backend.tf
@@ -1,8 +1,8 @@
-#terraform {
- # backend "azurerm" {
-  #  resource_group_name  = "Github-Pipeline"
-  #  storage_account_name = "githubpipeline060826"
-  #  container_name       = "tfstate"
-  #  key                  = "dev.tfstate"
- # }
-#}
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "Github-Pipeline"
+   storage_account_name = "githubpipeline060826"
+    container_name       = "tfstate"
+    key                  = "dev.tfstate"
+  }
+}
